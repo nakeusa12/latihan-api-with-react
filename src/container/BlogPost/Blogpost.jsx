d988350f70b43e0b4801fd4f8af93b11a45999f9
nakeusa12 <snakeusa8@gmail.com>
2020-07-08 01:20:24 +0700

    Latihan GET API

---
+++ src/container/BlogPost/Blogpost.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./Blogpost.css";
+import Post from "../../Component/Post/Post";
+import axios from "axios";
+
+class Blogpost extends Component {
+  state = {
+    post: [],
+  };
+
+  componentDidMount() {
+    // ? fetch = standar es6
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     this.setState({
+    //       post: json,
+    //     });
+    //   });
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      this.setState({
+        post: res.data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <p className="section-title">Blog</p>
+        {this.state.post.map((post) => {
+          return <Post key={post.id} title={post.title} desc={post.body} />;
+        })}
+      </>
+    );
+  }
+}
+
+export default Blogpost;
