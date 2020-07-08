590f6e99d11e56944ac21434fa8ba7931cfb2e38
nakeusa12 <snakeusa8@gmail.com>
2020-07-09 00:02:12 +0700

    POST API

--- src/container/BlogPost/Blogpost.jsx
+++ src/container/BlogPost/Blogpost.jsx
@@ -6,6 +6,12 @@ import axios from "axios";
 class Blogpost extends Component {
   state = {
     post: [],
+    formBlogPost: {
+      id: 1,
+      title: "",
+      body: "",
+      userId: 1,
+    },
   };
 
   getPostAPI = () => {
@@ -22,15 +28,21 @@ class Blogpost extends Component {
     });
   };
 
+  handleFormChange = (event) => {
+    let formBlogPostNew = { ...this.state.formBlogPost };
+    formBlogPostNew[event.target.name] = event.target.value;
+    let title = event.target.value;
+    this.setState(
+      {
+        formBlogPost: title,
+      },
+      () => {
+        console.log(this.state.formBlogPost);
+      }
+    );
+  };
+
   componentDidMount() {
-    // ? fetch = standar es6
-    // fetch("https://jsonplaceholder.typicode.com/posts")
-    //   .then((response) => response.json())
-    //   .then((json) => {
-    //     this.setState({
-    //       post: json,
-    //     });
-    //   });
     this.getPostAPI();
   }
 
@@ -38,6 +50,26 @@ class Blogpost extends Component {
     return (
       <>
         <p className="section-title">Blog</p>
+        <div className="form-add-post">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            name="title"
+            placeholder="add title"
+            onChange={this.handleFormChange}
+          />
+          <label htmlFor="body">Blog Content</label>
+          <textarea
+            name="body"
+            id="body"
+            cols="30"
+            rows="10"
+            placeholder="Add blog desc"
+            onChange={this.handleFormChange}
+          />
+
+          <button className="btn-submit">Simpan</button>
+        </div>
         {this.state.post.map((post) => {
           return <Post key={post.id} data={post} remove={this.handleRemove} />;
         })}
