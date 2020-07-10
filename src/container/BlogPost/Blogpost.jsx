ca9b37efebf79f097905ebeb3a7798eb152083e8
nakeusa12 <snakeusa8@gmail.com>
2020-07-10 22:26:46 +0700

    PUT FakeAPI

--- src/container/BlogPost/Blogpost.jsx
+++ src/container/BlogPost/Blogpost.jsx
@@ -12,8 +12,11 @@ class Blogpost extends Component {
       body: "",
       userId: 1,
     },
+    isUpdate: false,
   };
 
+  // Mengambil data API
+  // _sort=id&_order=desc digunakan untuk memuculkan data terbaru
   getPostAPI = () => {
     axios
       .get("http://localhost:3004/posts?_sort=id&_order=desc")
@@ -28,26 +31,71 @@ class Blogpost extends Component {
   postDataToAPI = () => {
     axios
       .post("http://localhost:3004/posts", this.state.formBlogPost)
-      .then((res) => {
-        console.log(res);
+      .then(() => {
         this.getPostAPI();
+        this.setState({
+          formBlogPost: {
+            id: 1,
+            title: "",
+            body: "",
+            userId: 1,
+          },
+        });
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  // Fungsi untuk menyimpan kembali data yang sudah diubah kedalam API
+  putDataAPI = () => {
+    axios
+      .put(
+        `http://localhost:3004/posts/${this.state.formBlogPost.id}`,
+        this.state.formBlogPost
+      )
+      .then(() => {
+        this.getPostAPI();
+        this.setState({
+          isUpdate: false,
+          formBlogPost: {
+            id: 1,
+            title: "",
+            body: "",
+            userId: 1,
+          },
+        });
+      });
+  };
+
+  // Method untuk menghapus data pada API
   handleRemove = (data) => {
     axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
       this.getPostAPI();
     });
   };
 
-  handleFormChange = (event) => {
-    let formBlogPostNew = { ...this.state.formBlogPost };
-    let timeStamp = new Date().getTime();
-    formBlogPostNew["id"] = timeStamp;
+  // Method untuk mengupdate data pada API
+  handleUpdate = (data) => {
+    this.setState({
+      formBlogPost: data,
+      isUpdate: true,
+    });
+  };
 
+  handleFormChange = (event) => {
+    // mengambil isi dari objek fromblogpost
+    let formBlogPostNew = { ...this.state.formBlogPost };
+
+    // timestamp dijadikan Id saat isi form ditambahkan
+    let timeStamp = new Date().getTime();
+
+    // jika form dalam kondisi post maka jalankan dibawah ini, namun jika form dalam kondisi update jangan jalankan timestap id agar Id dari postnya tidak terubah
+    if (!this.state.isUpdate) {
+      formBlogPostNew["id"] = timeStamp;
+    }
+
+    // menargetkan kedua nilai name pada form input agar ketika di isi, data tersebut bisa diupdate ke dalam state formBlogPost
     formBlogPostNew[event.target.name] = event.target.value;
     this.setState({
       formBlogPost: formBlogPostNew,
@@ -56,9 +104,15 @@ class Blogpost extends Component {
 
   // method submit untuk mengirimnnya
   handleSubmit = () => {
-    this.postDataToAPI();
+    // Jika isUpdate(pada state) bernilai true makan jalankan putDataApi(), namun jika tetap false jalanlan postDataToAPI()
+    if (this.state.isUpdate) {
+      this.putDataAPI();
+    } else {
+      this.postDataToAPI();
+    }
   };
 
+  // lifecycle pada react untuk mengupdate
   componentDidMount() {
     this.getPostAPI();
   }
@@ -73,6 +127,7 @@ class Blogpost extends Component {
             type="text"
             name="title"
             placeholder="add title"
+            value={this.state.formBlogPost.title}
             onChange={this.handleFormChange}
           />
           <label htmlFor="body">Blog Content</label>
@@ -82,6 +137,7 @@ class Blogpost extends Component {
             cols="30"
             rows="10"
             placeholder="Add blog desc"
+            value={this.state.formBlogPost.body}
             onChange={this.handleFormChange}
           />
 
@@ -95,7 +151,14 @@ class Blogpost extends Component {
           </button>
         </div>
         {this.state.post.map((post) => {
-          return <Post key={post.id} data={post} remove={this.handleRemove} />;
+          return (
+            <Post
+              key={post.id}
+              data={post}
+              remove={this.handleRemove}
+              update={this.handleUpdate}
+            />
+          );
         })}
       </>
     );
